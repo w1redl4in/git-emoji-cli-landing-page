74eb158069136fd172b146aa7b1f4d00eff9a759
w1redl4in <felipe.austriaco@mercadolivre.com>
2022-09-17 02:07:33 -0300

    feat: :sparkles: add hero component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+export function Hero() {
+  return (
+    <Box
+      minH="100vh"
+      backgroundPosition="center"
+      backgroundImage="url('hero.jpg')"
+    />
+  );
+}
