bce330e1c2544c3a32829502075a5f4abe5d061c
w1redl4in <felipe.austriaco@mercadolivre.com>
2022-09-24 15:12:22 -0300

    refactor(collaborators): :recycle: using array structure

--- src/common/collaborators/index.tsx
+++ src/common/collaborators/index.tsx
@@ -1,6 +1,29 @@
-import { Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import { Flex, Heading, Image, Link, Text, VStack } from "@chakra-ui/react";
+import { useMemo } from "react";
 
 export function Collaborators() {
+  const collaborators = useMemo(
+    () => [
+      {
+        name: "Felipe Austríaco",
+        type: "Criador",
+        company: "Software Engineer @ Mercado Livre",
+        peep: "felipe.svg",
+        peepAlt: "Um peep que representa o Felipe",
+        link: "https://felipeaustriaco.dev",
+      },
+      {
+        name: "Vitor Delfino",
+        type: "Commiter",
+        company: "Software Engineer @ Loft",
+        peep: "vitao.svg",
+        peepAlt: "Um peep que representa o Vitor",
+        link: "https://vitordelfino.dev",
+      },
+    ],
+    []
+  );
+
   return (
     <Flex
       minH="100vh"
@@ -10,20 +33,29 @@ export function Collaborators() {
     >
       <Heading fontSize="5xl">Colaboradores</Heading>
 
-      <Flex w="100%" justifyContent="space-around" flexWrap="wrap">
-        <VStack>
-          <Heading fontSize="5xl">Criador</Heading>
-          <Image src="felipe.svg" alt="On Peeps de mim" />
-          <Heading fontSize="3xl">Felipe Austríaco</Heading>
-          <Text>Software Engineer @ Mercado Livre</Text>
-        </VStack>
-
-        <VStack>
-          <Heading fontSize="5xl">Commiter</Heading>
-          <Image src="vitao.svg" alt="On Peeps de mim" />
-          <Heading fontSize="3xl"> Vitor Delfino</Heading>
-          <Text>Software Engineer @ Loft</Text>
-        </VStack>
+      <Flex mb="5" w="100%" justifyContent="space-around" flexWrap="wrap">
+        {collaborators.map((collaborator) => (
+          <VStack key={collaborator.name}>
+            <Image
+              maxW="20rem"
+              w="100%"
+              h="auto"
+              src={collaborator.peep}
+              alt={collaborator.peepAlt}
+            />
+            <Heading fontSize="5xl">{collaborator.type}</Heading>
+            <Heading fontSize="3xl">
+              <Link
+                textDecor="underline"
+                href={collaborator.link}
+                target="_blank"
+              >
+                {collaborator.name}
+              </Link>
+            </Heading>
+            <Text>{collaborator.company}</Text>
+          </VStack>
+        ))}
       </Flex>
     </Flex>
   );
